f377eb8794d072a5adb4ab98d9266606d5ea4093
DESKTOP-9F5APHK\Admin <levantung0000@gmail.com>
2020-11-12 22:32:04 +0700

    bai 6 lần 3

---
+++ TungShop.Data/Infrastructure/Dbfactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TungShop.Model.Infrastructure;
+
+namespace TungShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        public TungshopDbContext Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
